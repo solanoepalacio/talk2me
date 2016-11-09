30a7ce7a142ae4f22c778284e2bf5a48288ef318
Solano <solanoepalacio@gmail.com>
2016-11-08 21:05:03 -0300

    Converted to php
    Converted index.html into index.php to start working on the php
    Created the login and signup html/css basic structure
    Started working on the pasword validation with js

--- index.php
+++ index.php
@@ -8,17 +8,69 @@
     <!-- cambie todas las tipos a helvetica por ahora, mepa que le vamos a dar una estetica un toque mas seria, asi q no tamos usando esas tipos-->
         <script type='text/javascript' src='script.js'></script>
         
+        <?php
+        //variable definitions:
+        $loginError = '';
+        $signupError= '';
+
+
+
+        ?>
+
 
     </head>
 <body>
-    <div id="container">
+    <div id="loginFormContainer">
+      <div id="loginFormContent">
+        <div id="loginFormHeader">
+          <h2>Titulo del login</h2><span id="close" onclick="closeLogin()">X</span>
+        </div>
+
+        <div id="loginFormBody">
+          <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST">
+            <input class="loginInput" type="text" name="userName" placeholder="User name">
+            <input class="loginInput" type="password" name="password" placeholder="Password">
+            <p id="loginError"><?php echo $loginError; ?></p>
+            <center><input id="loginSubmit" type="submit" value="Log in"></center>
+        </div>
+
+        <div id="loginFormFooter">
+          <p>Click here for more info?</p>
+        </div>
+      </div>
+    </div>
+
+    <div id="signupFormContainer">
+      <div id="signupFormContent">
+        <div id="signupFormHeader">
+          <h2>Titulo del signup</h2><span id="close" onclick="closeSignup()">X</span>
+        </div>
+
+        <div id="signupFormBody">
+          <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST">
+            <input class="signupInput" type="text" name="userEmail" placeholder="E-mail">
+            <input class="signupInput" type="text" name="userName" placeholder="User name">
+            <input class="signupInput" id="signupPsw" type="password" name="password" placeholder="Password">
+            <input class="signupInput" id="signupPswRpt" type="password" name="passwordRepeat" placeholder="Repeat Password" onkeydown="setInterval(checkPswd, 300)">
+            <p id="paswError"></p>
+            <center><input id="signupSubmit" type="submit" value="Sign Up" disabled="true"></center>
+        </div>
+
+        <div id="signupFormFooter">
+          <a href="#">Click here for more info?</a>
+        </div>
+      </div>
+    </div>
+
+
+    <div id="MainContainer">
       <div id="header">
         <div id="headerContainer">
           <div id="brandContainer"> <a href='index.html'><h1>Talk 2 Me</h1></a></div>
           <form action="./search.php" id="searchBar"><input type="text" name="searchKeyword" id="searchField" placeholder="search"></form>
           <div id="loginContainer">
-            <p class="bttn loginBttn" id="login">Log-in</p>
-            <p class="bttn loginBttn" id="signup">Sign-Up</p>
+            <p class="bttn loginBttn" id="login" onclick="openLogin()">Log-in</p>
+            <p class="bttn loginBttn" id="signup" onclick="openSignup()">Sign-Up</p>
           </div>
         </div>
       </div>
@@ -63,14 +115,16 @@
                 <p>Enter a word to search as a trigger for a conversation with me!</p>
 
                 <p>I will tell you about the term, according by what I understand by it :)</p>
-                <center> <div id="prompt_container">
+                <div id="prompt_container">
 
 
-<input id="questionField" type="text" name="question" onkeypress="isEnter()"> <!--el onsubmit no funciona, apretando enter no dispara la opcion, solo dandole clic al boton que esta afuera del form (agregue un botn adentro del form con las mismas propiedades y no funciona) -->
+                  <input id="questionField" type="text" name="question" onkeypress="isEnter()"> <!--el onsubmit no funciona, apretando enter no dispara la opcion, solo dandole clic al boton que esta afuera del form (agregue un botn adentro del form con las mismas propiedades y no funciona)
+                  Solano 08/011: ya le vamos a sacar la ficha de como hacerlo. Lo que estoy intentando es utilizar la funcion
+                  onkeypress y adentro de esa función usar key.code() para saber si la tecla que se apretó fue enter. 
+                  cualquier cosa pedime y te paso el link de donde saque la idea -->
+                  <button onclick="questions()">Submit</button>
 
-<button onclick="questions()">Submit</button>
-
-
+                </div>
               </div>
 
               <div class="relCon_section" id="relCon_alsoLike">
---
+++ script.js
@@ -0,0 +1,105 @@
+function openLogin(){
+  document.getElementById("loginFormContainer").style="display:block";
+}
+function closeLogin(){
+  document.getElementById("loginFormContainer").style="display:none";
+}
+function openSignup(){
+  document.getElementById("signupFormContainer").style="display:block";
+}
+function closeSignup(){
+  document.getElementById("signupFormContainer").style="display:none";
+}
+function checkPswd(){
+  var psw = document.getElementById("signupPsw").value;
+  var pswR = document.getElementById("signupPswRpt").value;
+  document.getElementById("signupSubmit").disabled= true;
+  if (psw == pswR){
+    document.getElementById("paswError").innerHTML="";
+    document.getElementById("signupSubmit").disabled= false;
+  } else{
+    document.getElementById("paswError").innerHTML="The passwords don't match";
+  }
+
+
+}
+
+
+
+function linkcases(caseNumber){
+  switch (caseNumber){
+    case 1:
+      document.getElementById("videoPlayer").src="https://www.youtube.com/embed/YLGx5Pbcbuk"; //Ostera dormido
+      break;
+    case 2:
+      document.getElementById("videoPlayer").src="https://www.youtube.com/embed/yO0gHLGfd8Q"; //Caminos del norte
+      break;
+    case 3:
+      document.getElementById("videoPlayer").src="https://www.youtube.com/embed/KIVeSDSu_M4"; //Hernan Ruiz Diaz
+      break;
+    case 4:
+      document.getElementById("videoPlayer").src="https://www.youtube.com/embed/JD2Zw7PayoU"; //Pato Sanchez
+      break;
+    case 5:
+      document.getElementById("videoPlayer").src="https://www.youtube.com/embed/L5bU9tb417I"; //Manu Pellagata
+      break;
+    case 6:
+      document.getElementById("videoPlayer").src="https://www.youtube.com/embed/WeVvR_ESOs8"; //Tom Craig
+      break;
+    case 7:
+      document.getElementById("videoPlayer").src="https://www.youtube.com/embed/G9LsEH8kbT4"; //Kiwi
+      break;
+    case 8:
+      document.getElementById("videoPlayer").src="https://www.youtube.com/embed/cJj9uBv0qbI"; //Tess Parks
+      break;
+    case 9:
+      document.getElementById("videoPlayer").src="https://www.youtube.com/embed/YQhgEbPo2mY"; // Allah Lahs
+      break;
+  }
+}
+
+function isEnter(event){
+
+  var keyPressed = event.keyCode();
+  alert("hola");
+
+  if (keyPressed == 13) {
+    alert("inside the if");
+    questions(document.getElementById('questionField').value)  //cambié pregunta por questionField para que quede todo en el mismo idioma.
+    return false;                                              //lo puse en inglés por si alguien se dispone a ayudarnos en github
+  }
+}
+
+
+function questions(question){
+  var question = document.getElementById('questionField').value
+  switch (question){
+    case "why":
+      document.getElementById("videoPlayer").src="https://www.youtube.com/embed/YLGx5Pbcbuk"; //Ostera dormido
+      break;
+    case "how":
+      document.getElementById("videoPlayer").src="https://www.youtube.com/embed/yO0gHLGfd8Q"; //Caminos del norte
+      break;
+    case "who":
+      document.getElementById("videoPlayer").src="https://www.youtube.com/embed/KIVeSDSu_M4"; //Hernan Ruiz Diaz
+      break;
+    case "where":
+      document.getElementById("videoPlayer").src="https://www.youtube.com/embed/JD2Zw7PayoU"; //Pato Sanchez
+      break;
+    case "name":
+      document.getElementById("videoPlayer").src="https://www.youtube.com/embed/L5bU9tb417I"; //Manu Pellagata
+      break;
+    case "when":
+      document.getElementById("videoPlayer").src="https://www.youtube.com/embed/WeVvR_ESOs8"; //Tom Craig
+      break;
+    case 'what for':
+      document.getElementById("videoPlayer").src="https://www.youtube.com/embed/G9LsEH8kbT4"; //Kiwi
+      break;
+    case 'what':
+      document.getElementById("videoPlayer").src="https://www.youtube.com/embed/cJj9uBv0qbI"; //Tess Parks
+      break;
+    default:
+      document.getElementById("videoPlayer").src="https://www.youtube.com/embed/YQhgEbPo2mY"; // Allah Lahs
+      break;
+  }
+}
